1df010d49f3fd3a0fa6610f17b9c11cb7505be9f
Benjamin Uekermann <benjamin.uekermann@gmail.com>
2020-11-04 08:54:25 +0100

    Update documentation of corrosion FEniCS code

--- corrosion.py
+++ corrosion.py
@@ -3,30 +3,32 @@ from fenicsprecice import Adapter
 
 mesh = Mesh('Gapmesh.xml')
 x = mesh.coordinates()
-x[:, :] *= 0.001
+x[:, :] *= 0.001 # mesh is scaled from m to mm to better fit with the existing fracture code
 
 V = FunctionSpace(mesh, "Lagrange", 1)
 
 def boundary(x):
     return x[0] < 0.001 + DOLFIN_EPS or x[0] > 0.0015 - DOLFIN_EPS
     
-# define coupling domain
+# define coupling domain as complete domain
 def coupling(x):
     return True    
 
 dim = 2
 coupling_domain = AutoSubDomain(coupling)
 
+# the FEniCS-preCICE adapter is configured via a json file. In there, we also need to specify some arbitrary (unused) read data.
+# We are currently trying to get rid of this technical restriction.
 precice = Adapter(adapter_config_filename="precice-adapter-config.json")
 
-precice_dt = precice.initialize(coupling_domain, mesh, V, dim)
+precice_dt = precice.initialize(coupling_domain, mesh, V, dim) # here we define where the coupling should happen
 
     
-# Define boundary condition
+# Define boundary condition on left and right boundary
 u0 = Constant(0.0)
 bc = DirichletBC(V, u0, boundary)
 
-# Define variational problem
+# Define variational problem, simply copied from FEniCS demo
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2)
@@ -34,7 +36,7 @@ g = Expression("sin(5*x[0])", degree=2)
 a = inner(grad(u), grad(v))*dx
 L = f*v*dx + g*v*ds
 
-while precice.is_coupling_ongoing():
+while precice.is_coupling_ongoing(): # potential time loop, currently only one timestep is done
 
   # Compute solution
   u = Function(V)
@@ -44,6 +46,7 @@ while precice.is_coupling_ongoing():
   file = File("poisson.pvd")
   file << u
   
+  # write data to preCICE and advance coupling  
   precice.write_data(u)
   precice.advance(precice_dt)
 
